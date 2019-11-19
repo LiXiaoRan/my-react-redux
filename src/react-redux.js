989c92aac2092f7660abb124a8802a5004b015d0
997843911@qq.com <997843911@qq.com>
2019-11-19 19:37:44 +0800

    完成了我的react-redux渲染header和content的操作

--- src/react-redux.js
+++ src/react-redux.js
@@ -6,14 +6,39 @@ export const connect = (mapStateToProps) =>(WrappedComponent) => {
       static contextTypes = {
         store: PropTypes.object
       }
+
+      constructor(props) {
+        super(props);
+        this.state={
+          allProps:{}
+        }
+      }
       
+      
+      componentWillMount(){
+        let {store}=this.context;
+        this._updateProps();
+        store.subScribe(()=>{this._updateProps()})
+
+    }
+
+    _updateProps(){
+        let {store}=this.context;
+        let stateProps=mapStateToProps(store.getState(),this.props);// 额外传入 props，让获取数据更加灵活方便(不能让被包裹的组件原本的props没了)
+        this.setState({
+          allProps:{// 整合普通的 props 和从 state 生成的 props
+            ...stateProps,
+            ...this.props
+          }
+        })
+    }
 
       render () {
         //从 store 取数据
-        const {store}=this.context;
-        let stateProps=mapStateToProps(store.getState());
+        // const {store}=this.context;
+        // let stateProps=mapStateToProps(store.getState());
         // {...stateProps} 意思是把这个对象里面的属性全部通过 `props` 方式传递进去
-        return <WrappedComponent {...stateProps}/>
+        return <WrappedComponent {...this.state.allProps}/>
       }
     }
   
