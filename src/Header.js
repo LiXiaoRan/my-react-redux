d19000664d7744f168cdfc288313bad05934d9ff
997843911@qq.com <997843911@qq.com>
2019-11-19 14:35:40 +0800

    基本框架

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class Header extends Component{
+    constructor(props) {
+        super(props);
+        
+    }
+    
+
+    render() {
+        return (
+            <div>
+                  <h1>React.js 小书</h1>
+            </div>
+        );
+    }
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Component } from 'react'
 import './index.css';
-import App from './App';
+import Header from './Header'
+import Content from './Content'
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Index extends Component{
+    render() {
+        return (
+            <div>
+                <Header/>
+                <Content/>
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(<Index />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
