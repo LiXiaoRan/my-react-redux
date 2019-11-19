25d289aeb48401fa4b96a5e6ffcf71d2ac822b27
997843911@qq.com <997843911@qq.com>
2019-11-19 15:21:19 +0800

    Header中通过context获获取到store中的themecolor并渲染

--- src/Header.js
+++ src/Header.js
@@ -2,16 +2,31 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 class Header extends Component{
+    static contextTypes={
+        store:PropTypes.object
+    }
+
     constructor(props) {
         super(props);
-        
+        this.state={
+            themeColor:''
+        }
     }
     
+    componentWillMount(){
+        this._updateThemeColor();
+    }
+
+    _updateThemeColor(){
+        let {store}=this.context;
+        let state=store.getState();
+        this.setState({themeColor:state.themeColor})
+    }
 
     render() {
         return (
             <div>
-                  <h1>React.js 小书</h1>
+                  <h1 style={{color:this.state.themeColor}}>React.js 小书</h1>
             </div>
         );
     }
--- src/ThemeSwitch.js
+++ src/ThemeSwitch.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 class ThemeSwitch extends Component{
+    static contextTypes={
+        store:PropTypes.object
+    }
+
+    constructor(props) {
+        super(props);
+        this.state={
+            
+        }
+    }
+    
     render() {
         return (
             <div>
